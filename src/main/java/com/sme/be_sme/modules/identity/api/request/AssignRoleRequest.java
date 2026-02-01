40fcfe70535c87b7ad9458205056ceb0839a7103
anhphan237 <anh713988@gmail.com>
2026-02-01 18:13:17 +0700

    Get user detail

--- src/main/java/com/sme/be_sme/modules/identity/api/request/AssignRoleRequest.java
+++ src/main/java/com/sme/be_sme/modules/identity/api/request/AssignRoleRequest.java
@@ -2,8 +2,10 @@ package com.sme.be_sme.modules.identity.api.request;
 
 import lombok.*;
 
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AssignRoleRequest {
     private String userId;
     private String roleCode; // ADMIN/HR/MANAGER/IT/EMPLOYEE
---
+++ src/main/java/com/sme/be_sme/modules/identity/api/request/GetUserRequest.java
@@ -0,0 +1,17 @@
+package com.sme.be_sme.modules.identity.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GetUserRequest {
+
+    private String userId;
+}
